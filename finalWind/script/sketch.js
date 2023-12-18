061de8aa4b98ab12c1a91ded8f1e913a116894a0
kaelee23 <dahnlee@naver.com>
2023-12-18 14:56:48 +0900

    cc

--- finalWind/script/sketch.js
+++ finalWind/script/sketch.js
@@ -1,25 +1,24 @@
-let circlePositions = [];
+let circlePos = [];
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
 
   background(0);
 
-  storeInitialPositions();
+  //여기서부터
+  wordPlace();
 }
 function mouseClicked() {
   shuffleCharacterPositions();
 }
 
 function shuffleCharacterPositions() {
-  for (let i = circlePositions.length - 1; i > 0; i--) {
+  for (let i = circlePos.length - 1; i > 0; i--) {
     const j = floor(random(i + 1));
-    [circlePositions[i], circlePositions[j]] = [
-      circlePositions[j],
-      circlePositions[i],
-    ];
+    [circlePos[i], circlePos[j]] = [circlePos[j], circlePos[i]];
   }
 }
+//여기까지 gpt의 도움을 받았습니다.
 
 function draw() {
   background(0);
@@ -34,125 +33,120 @@ function draw() {
 
   circle(mouseX, mouseY, 50);
 
-  updateCirclePositions();
+  updateCirclePos();
 
-  drawCharacterW(circlePositions[0].x, circlePositions[0].y);
-  drawCharacterI(circlePositions[1].x, circlePositions[1].y);
-  drawCharacterN(circlePositions[2].x, circlePositions[2].y);
-  drawCharacterD(circlePositions[3].x, circlePositions[3].y);
+  drawW(circlePos[0].x, circlePos[0].y);
+  drawI(circlePos[1].x, circlePos[1].y);
+  drawN(circlePos[2].x, circlePos[2].y);
+  drawD(circlePos[3].x, circlePos[3].y);
 }
 
-function storeInitialPositions() {
-  circlePositions.push(createVector(width / 5, height / 2));
-  circlePositions.push(createVector((2 * width) / 5, height / 2));
-  circlePositions.push(createVector((3 * width) / 5, height / 2));
-  circlePositions.push(createVector((4 * width) / 5, height / 2));
+function wordPlace() {
+  circlePos.push(createVector(width / 5, height / 2));
+  circlePos.push(createVector((2 * width) / 5, height / 2));
+  circlePos.push(createVector((3 * width) / 5, height / 2));
+  circlePos.push(createVector((4 * width) / 5, height / 2));
 }
 
-function updateCirclePositions() {
-  for (let i = 0; i < circlePositions.length; i++) {
-    let distance = dist(
-      mouseX,
-      mouseY,
-      circlePositions[i].x,
-      circlePositions[i].y
-    );
+function updateCirclePos() {
+  for (let i = 0; i < circlePos.length; i++) {
+    let distance = dist(mouseX, mouseY, circlePos[i].x, circlePos[i].y);
 
     let maxDistance = 100;
 
     if (distance < maxDistance) {
       let direction = createVector(
-        circlePositions[i].x - mouseX,
-        circlePositions[i].y - mouseY
+        circlePos[i].x - mouseX,
+        circlePos[i].y - mouseY
       );
       direction.normalize();
       direction.mult(maxDistance - distance);
-      circlePositions[i].add(direction);
+      circlePos[i].add(direction);
     } else {
       let initialPosition = createVector(((i + 1) * width) / 5, height / 2);
-      let moveBack = p5.Vector.sub(initialPosition, circlePositions[i]);
+      let moveBack = p5.Vector.sub(initialPosition, circlePos[i]);
       moveBack.mult(0.05);
-      circlePositions[i].add(moveBack);
+      circlePos[i].add(moveBack);
     }
   }
 }
 
-function drawCharacterW(x, y) {
+function drawW(x, y) {
   fill(255);
   ellipse(constrain(x - 20, 0, width), y - 30, 4); //1
   ellipse(constrain(x - 20, 0, width), y - 25, 4); // 2
   ellipse(constrain(x - 20, 0, width), y - 20, 4); // 3
   ellipse(constrain(x - 20, 0, width), y - 15, 4); // 4
-  ellipse(constrain(x - 15, 0, width), y - 10, 4); // bottom-left
-  ellipse(constrain(x, 0, width), y - 15, 4); // middle-middle-left
-  ellipse(constrain(x + 20, 0, width), y - 30, 4); // top-right
-  ellipse(constrain(x + 20, 0, width), y - 25, 4); // top-right
-  ellipse(constrain(x + 20, 0, width), y - 20, 4); // top-right
-  ellipse(constrain(x + 20, 0, width), y - 15, 4); // top-right
-  ellipse(constrain(x - 10, 0, width), y - 5, 4); // bottom-middle-left
-  ellipse(constrain(x - 5, 0, width), y - 10, 4); // bottom-middle-left
-  ellipse(constrain(x + 5, 0, width), y - 10, 4); // bottom-middle-left
-  ellipse(constrain(x + 10, 0, width), y - 5, 4); // bottom-middle-right
-  ellipse(constrain(x + 15, 0, width), y - 10, 4); // bottom-right
+  ellipse(constrain(x - 15, 0, width), y - 10, 4);
+  ellipse(constrain(x, 0, width), y - 15, 4);
+  ellipse(constrain(x + 20, 0, width), y - 30, 4);
+  ellipse(constrain(x + 20, 0, width), y - 25, 4);
+  ellipse(constrain(x + 20, 0, width), y - 20, 4);
+  ellipse(constrain(x + 20, 0, width), y - 15, 4);
+  ellipse(constrain(x - 10, 0, width), y - 5, 4);
+  ellipse(constrain(x - 5, 0, width), y - 10, 4);
+  ellipse(constrain(x + 5, 0, width), y - 10, 4);
+  ellipse(constrain(x + 10, 0, width), y - 5, 4);
+  ellipse(constrain(x + 15, 0, width), y - 10, 4);
 }
 
-function drawCharacterI(x, y) {
+function drawI(x, y) {
   fill(255);
-  ellipse(constrain(x, 0, width), y - 30, 4); // top
-  ellipse(constrain(x - 5, 0, width), y - 30, 4); // top
-  ellipse(constrain(x + 5, 0, width), y - 30, 4); // top
-  ellipse(constrain(x, 0, width), y - 25, 4); // top-middle
-  ellipse(constrain(x, 0, width), y - 20, 4); // middle
-  ellipse(constrain(x, 0, width), y - 15, 4); // middle-middle
-  ellipse(constrain(x, 0, width), y - 10, 4); // bottom
-  ellipse(constrain(x, 0, width), y - 5, 4); // bottom-middle
-  ellipse(constrain(x, 0, width), y, 4); // bottom
-  ellipse(constrain(x - 5, 0, width), y, 4); // bottom
-  ellipse(constrain(x + 5, 0, width), y, 4); // bottom
+  ellipse(constrain(x, 0, width), y - 30, 4);
+  ellipse(constrain(x - 5, 0, width), y - 30, 4);
+  ellipse(constrain(x + 5, 0, width), y - 30, 4);
+  ellipse(constrain(x, 0, width), y - 25, 4);
+  ellipse(constrain(x, 0, width), y - 20, 4);
+  ellipse(constrain(x, 0, width), y - 15, 4);
+  ellipse(constrain(x, 0, width), y - 10, 4);
+  ellipse(constrain(x, 0, width), y - 5, 4);
+  ellipse(constrain(x, 0, width), y, 4);
+  ellipse(constrain(x - 5, 0, width), y, 4);
+  ellipse(constrain(x + 5, 0, width), y, 4);
 }
 
-function drawCharacterN(x, y) {
+function drawN(x, y) {
   fill(255);
-  ellipse(constrain(x - 15, 0, width), y - 30, 4); // left-top
-  ellipse(constrain(x - 15, 0, width), y - 25, 4); // left-top-middle
-  ellipse(constrain(x - 15, 0, width), y - 20, 4); // left-middle
-  ellipse(constrain(x - 15, 0, width), y - 15, 4); // left-middle-middle
-  ellipse(constrain(x - 15, 0, width), y - 10, 4); // left-bottom
-  ellipse(constrain(x - 15, 0, width), y - 5, 4); // left-bottom-middle
-  ellipse(constrain(x - 15, 0, width), y, 4); // left-bottom
+  ellipse(constrain(x - 15, 0, width), y - 30, 4);
+  ellipse(constrain(x - 15, 0, width), y - 25, 4);
+  ellipse(constrain(x - 15, 0, width), y - 20, 4);
+  ellipse(constrain(x - 15, 0, width), y - 15, 4);
+  ellipse(constrain(x - 15, 0, width), y - 10, 4);
+  ellipse(constrain(x - 15, 0, width), y - 5, 4);
+  ellipse(constrain(x - 15, 0, width), y, 4);
 
-  ellipse(constrain(x - 10, 0, width), y - 25, 4); // middle-middle-middle
-  ellipse(constrain(x - 5, 0, width), y - 20, 4); // middle-middle-middle
-  ellipse(constrain(x, 0, width), y - 15, 4); // middle-middle-middle
-  ellipse(constrain(x + 5, 0, width), y - 10, 4); // middle-middle-middle
-  ellipse(constrain(x + 10, 0, width), y - 5, 4); // middle-middle-middle
+  ellipse(constrain(x - 10, 0, width), y - 25, 4);
+  ellipse(constrain(x - 5, 0, width), y - 20, 4);
+  ellipse(constrain(x, 0, width), y - 15, 4);
+  ellipse(constrain(x + 5, 0, width), y - 10, 4);
+  ellipse(constrain(x + 10, 0, width), y - 5, 4);
 
-  ellipse(constrain(x + 15, 0, width), y - 30, 4); // right-top
-  ellipse(constrain(x + 15, 0, width), y - 25, 4); // right-top-middle
-  ellipse(constrain(x + 15, 0, width), y - 20, 4); // right-middle
-  ellipse(constrain(x + 15, 0, width), y - 15, 4); // right-middle-middle
-  ellipse(constrain(x + 15, 0, width), y - 10, 4); // right-bottom
-  ellipse(constrain(x + 15, 0, width), y - 5, 4); // right-bottom-middle
-  ellipse(constrain(x + 15, 0, width), y, 4); // right-bottom
+  ellipse(constrain(x + 15, 0, width), y - 30, 4);
+  ellipse(constrain(x + 15, 0, width), y - 25, 4);
+  ellipse(constrain(x + 15, 0, width), y - 20, 4);
+  ellipse(constrain(x + 15, 0, width), y - 15, 4);
+  ellipse(constrain(x + 15, 0, width), y - 10, 4);
+  ellipse(constrain(x + 15, 0, width), y - 5, 4);
+  ellipse(constrain(x + 15, 0, width), y, 4);
 }
 
-function drawCharacterD(x, y) {
+function drawD(x, y) {
   fill(255);
-  ellipse(constrain(x - 15, 0, width), y - 30, 4); // left-top
-  ellipse(constrain(x - 15, 0, width), y - 25, 4); // left-top-middle
-  ellipse(constrain(x - 15, 0, width), y - 20, 4); // left-middle
-  ellipse(constrain(x - 15, 0, width), y - 15, 4); // left-middle-middle
-  ellipse(constrain(x - 15, 0, width), y - 10, 4); // left-bottom
-  ellipse(constrain(x - 15, 0, width), y - 5, 4); // left-bottom-middle
-  ellipse(constrain(x - 15, 0, width), y, 4); // left-bottom
-  ellipse(x - 10, y - 30, 4); // top-middle
-  ellipse(x - 5, y - 30, 4); // top-middle
-  ellipse(x - 5, y - 30, 4); // top-middle
-  ellipse(x, y - 25, 4); // top-middle
-  ellipse(x + 5, y - 20, 4); // top-middle
-  ellipse(x + 5, y - 15, 4); // top-middle
-  ellipse(x + 5, y - 10, 4); // top-middle
-  ellipse(x, y - 5, 4); // top-middle
-  ellipse(x - 5, y, 4); // top-middle
-  ellipse(x - 10, y, 4); // top-middle
+  ellipse(constrain(x - 15, 0, width), y - 30, 4);
+  ellipse(constrain(x - 15, 0, width), y - 25, 4);
+  ellipse(constrain(x - 15, 0, width), y - 20, 4);
+  ellipse(constrain(x - 15, 0, width), y - 15, 4);
+  ellipse(constrain(x - 15, 0, width), y - 10, 4);
+  ellipse(constrain(x - 15, 0, width), y - 5, 4);
+  ellipse(constrain(x - 15, 0, width), y, 4);
+  ellipse(x - 10, y - 30, 4);
+  ellipse(x - 5, y - 30, 4);
+  ellipse(x - 5, y - 30, 4);
+  ellipse(x, y - 25, 4);
+  ellipse(x + 5, y - 20, 4);
+  ellipse(x + 5, y - 15, 4);
+  ellipse(x + 5, y - 10, 4);
+  ellipse(x, y - 5, 4);
+  ellipse(x - 5, y, 4);
+  ellipse(x - 10, y, 4);
 }
